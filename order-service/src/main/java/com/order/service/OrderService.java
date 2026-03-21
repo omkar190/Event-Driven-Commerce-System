b36ac20a269e3d1454180b9af5d71c3819f12a04
Omkar <omkar02d@gmail.com>
2026-03-21 12:47:26 +0530

    Added application.yml, application-dev.yml and added DB settings.

--- order-service/src/main/java/com/order/service/OrderService.java
+++ order-service/src/main/java/com/order/service/OrderService.java
@@ -2,9 +2,8 @@ package com.order.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.r2dbc.autoconfigure.R2dbcAutoConfiguration;
 
-@SpringBootApplication(exclude = {R2dbcAutoConfiguration.class})
+@SpringBootApplication
 public class OrderService {
 
     public static void main(String[] args) {
