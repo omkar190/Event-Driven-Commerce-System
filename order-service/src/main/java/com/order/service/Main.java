0444b20b0cfd41cef48e0b3b60fa50fb16f4c542
Omkar <omkar02d@gmail.com>
2026-05-02 00:40:36 +0530

    Added AIVEN Postgres DB Config

--- order-service/src/main/java/com/order/service/Main.java
+++ order-service/src/main/java/com/order/service/Main.java
@@ -3,10 +3,13 @@ package com.order.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(Main.class, args);
         System.out.println("--- Spring Boot Application Started Successfully! ---");
     }
