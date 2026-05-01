19b62ac0c2a3b49548fe0237a62ed978f6788f12
Omkar <omkar02d@gmail.com>
2026-05-02 02:30:08 +0530

    Fixed redirect for homepage.

--- order-service/src/main/java/com/order/service/controllers/RedirectController.java
+++ order-service/src/main/java/com/order/service/controllers/RedirectController.java
@@ -1,14 +1,20 @@
 package com.order.service.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import reactor.core.publisher.Mono;
+import org.springframework.web.server.ServerWebExchange;
 
 @RestController
 public class RedirectController {
-    @GetMapping(value = {"/"})
-    public void redirect(HttpServletResponse response) throws IOException {
-        response.sendRedirect("https://order-ui-y50d.onrender.com");
+
+    @GetMapping("/")
+    public Mono<Void> redirect(ServerWebExchange exchange) {
+        exchange.getResponse().setStatusCode(HttpStatus.FOUND);
+        exchange.getResponse().getHeaders().setLocation(
+                java.net.URI.create("https://order-ui-y50d.onrender.com")
+        );
+        return exchange.getResponse().setComplete();
     }
 }
